d0313a8feafa3487738eefaba50f590bbb9ebf44
gabcqw <134643615+gabcqw@users.noreply.github.com>
2023-07-07 21:00:47 +0100

    Add files via upload

--- schoolmgmt.py
+++ schoolmgmt.py
@@ -34,7 +34,7 @@ def create_account(option):
             }
             print("The student has been created.") 
             school_dict["student"].append(temp_dict)
-            break
+        
                       
         elif option == "teacher": #teacher_account():
             name = input("Provide the first name:  ")
@@ -48,7 +48,7 @@ def create_account(option):
             }
             print("The teacher has been created.")
             school_dict["teacher"].append(temp_dict)
-            break
+    
             
             
         elif option == "homeroom_teacher": #def homeroom_teacher_account():
@@ -61,16 +61,17 @@ def create_account(option):
             }
             print("The homeroom teacher has been created.")
             school_dict["homeroom_teacher"].append(temp_dict)
-            break
+        
         elif option == "end":
             break            
             
         else:
             print("It's invalid input! Please try again. ")
-            break
+
 
 
 def manage_account(option):
+    print("Enter user's type or enter class to see the whole class.")
     while True:
         option = input("Enter the user you tend to manage: ")
         if option == "student":
@@ -80,41 +81,51 @@ def manage_account(option):
                 if student['f_name'] == f_name and student['l_name'] == l_name:
                     print(f"{student['f_name']} {student['l_name']} attends class {student['class_attend']} ")
                     print("Student's teachers:")
-                    if student['class_attend'] == teacher['class_teach']:
-                        print(f"{teacher['f_name']} {teacher['l_name']} teaches this class")
+            for teacher in school_dict["teacher"]:
+                if student['class_attend'] == teacher['class_teach']:
+                        print(f"{teacher['f_name']} {teacher['l_name']} teaches this class {teacher['subject']}")
+    
 
         elif option == "teacher":
             f_name = input("Enter first name: ")
             l_name = input("Enter last name: ")
             for teacher in school_dict["teacher"]:
                 if teacher ['f_name'] == f_name and teacher['l_name'] == l_name:
-                    print(f"{teacher['f_name']} {teacher['l_name']} teaches class {student['class_attend']} ")
+                    print(f"{teacher['f_name']} {teacher['l_name']} teaches class {student['class_attend']} {teacher['subject']}")
+                    print("Student's list:")
+            for student in school_dict["student"]:
+                if student['class_attend'] == teacher['class_teach']:
+                    print(f"{student['f_name']} {student['l_name']} ")
+
 
         elif option == "homeroom_teacher":
             f_name = input("Enter first name: ")
             l_name = input("Enter last name: ")
             for homeroom_teacher in school_dict["homeroom_teacher"]:
                 if homeroom_teacher['f_name'] == f_name and homeroom_teacher['l_name'] == l_name:
-                    print(f"Homeroom teacher {f_name} {l_name} leads class {homeroom_teacher['class_lead']} ") #can i just put class_lead?
+                    print(f"Homeroom teacher {f_name} {l_name} leads class {homeroom_teacher['class_lead']} ") 
                     print("Student's list:")
-                    if student['class_attend'] == homeroom_teacher['class_lead']:
-                        print(f"{student['f_name']} {student['l_name']} ")
-        #how to print class as a variable? - 
-        #'class': Prompt for a class to display (e.g., "3C"), the program should list all students in the class and the homeroom teache
+            for student in school_dict["student"]:
+                if student['class_attend'] == homeroom_teacher['class_lead']:
+                    print(f"{student['f_name']} {student['l_name']} ")
+
+            
+        elif option == "class":
             search_class = input("Provide class for search: ")
             for student in school_dict["student"]:
+                print("Student's list:")
                 if student["class_attend"] == search_class:
-                    print(student)
-            for student in school_dict["homeroom_teacher"]:
+                    print(f"{student['f_name']} {student['l_name']} ")
+            for homeroom_teacher in school_dict["homeroom_teacher"]:
                 if homeroom_teacher["class_lead"] == search_class:
-                    print(homeroom_teacher)
+                    print(f"Homeroom teacher {f_name} {l_name} leads class {homeroom_teacher['class_lead']} ")
+
                 
         
         elif option == "end":
             break
         else:
             print("It's invalid input! Please try again. ")
-            break
 
 
 
@@ -125,7 +136,7 @@ while True:
     if option == "create":
         create_account (option) 
     elif option == "manage":
-        manage_account (option) 
+        manage_account(option) 
     elif option == "end":
         break
     else:
